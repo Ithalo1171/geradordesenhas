c010b25e6fa5588edb32cca1a1de96ce86a674b7
Ithalo Gabriel <144706141+Ithalo1171@users.noreply.github.com>
2023-09-15 15:47:23 -0300

    Gerador de senha
    Primeiro projeto usando HTML, CSS e Javascript. A ideia foi criar um projeto organizado com interação.

---
+++ script.js
@@ -0,0 +1,26 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function(){
+  sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+
+  let pass = "";
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+}
